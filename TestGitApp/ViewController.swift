c7c831476ce6b7e7f51ddbe0a743087d6c45e8e8
kota0207 <minato.innetwork@gmail.com>
2021-03-06 17:16:39 +0900

    Initial Commit

---
+++ TestGitApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestGitApp
+//
+//  Created by minato on 2021/03/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
